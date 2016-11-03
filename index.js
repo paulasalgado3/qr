c65f375fcff14e50e9f1c0f374840283aacac7d6
Paula Salgado <mail@algo.com>
2016-11-03 17:35:48 -0300

    creacion de codigo qr

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ var http = require('http');
 var https = require('https');
 var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
 var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+var qr = require ('qr-image');
+
+var code = qr.image('http://www.google.com', { type: 'svg' });
+var output = fs.createWriteStream('nodejitsu.svg')
+code.pipe(output);
 
 var credentials = {key: privateKey, cert: certificate};
 var express = require('express');
@@ -17,5 +22,8 @@ var httpsServer = https.createServer(credentials, app);
 httpsServer.listen(8443);
 
 app.get('/', function (req,res,next){
-	res.sendFile(__dirname + '/index.html');
+//	res.sendFile(__dirname + '/index.html');
+	 var code = qr.image(new Date().toString(), { type: 'svg' });
+  res.type('svg');
+  code.pipe(res);
 });
