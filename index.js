484848b1a8f4e44817812f14ec56b1caafa002c3
Paula Salgado <mail@algo.com>
2016-11-04 13:19:41 -0300

    registro remoto con id unico

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ var https = require('https');
 var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
 var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
 var qr = require ('qr-image');
-
-var code = qr.image('http://www.google.com', { type: 'svg' });
-var output = fs.createWriteStream('nodejitsu.svg')
-code.pipe(output);
+var fp = require ('fingerprintjs2');
+var bodyParser = require('body-parser');
 
 var credentials = {key: privateKey, cert: certificate};
 var express = require('express');
@@ -20,10 +18,34 @@ var httpsServer = https.createServer(credentials, app);
 
 //httpServer.listen(8080);
 httpsServer.listen(8443);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+        extended: true
+        }));
 
-app.get('/', function (req,res,next){
-//	res.sendFile(__dirname + '/index.html');
-	 var code = qr.image(new Date().toString(), { type: 'svg' });
-  res.type('svg');
-  code.pipe(res);
+app.get(/^(.+)$/, function(req,res,next){
+	switch(req.params[0]){
+		case '/registro':
+			var code = qr.image("https://10.105.231.63:8443/registroRemoto", { type: 'svg' });
+		        res.type('svg');
+        		code.pipe(res);
+			break;
+		case '/registroRemoto':
+			res.sendFile(__dirname + '/registroRemoto.html');
+			break;
+		case '/guardarRegistro':
+		default:
+			res.sendFile(__dirname + req.params[0]);		
+			break;
+	}
+});
+
+app.post(/^(.+)$/, function(req,res,next){
+	switch(req.params[0]){
+		case '/guardarRegistro':
+			console.log(req.body.id);
+			break;	
+		default:
+			break;
+	}
 });
